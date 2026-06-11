1f450b30d8efd1ab7dbe1f16dd0a37147659cf4d
Pavani Edirisinghe <eg245148@engug.ruh.ac.lk>
2026-06-11 13:01:05 +0530

    feat: Implement Virtual Painter with bitwise canvas masking and UI state selection

--- HandTrackingProject/HandTrackingModule.py
+++ HandTrackingProject/HandTrackingModule.py
@@ -13,6 +13,8 @@ class handDetector():
         self.hands = self.mpHands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands, min_detection_confidence=self.detectionCon, min_tracking_confidence=self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
+        self.tipIds = [4, 8, 12, 16, 20]
+
         # Landmark dots (Red)
         self.lmkStyle = self.mpDraw.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2)
         # Connection edges (Green)
@@ -31,7 +33,7 @@ class handDetector():
 
 
     def findPosition(self, img, handNo=0, draw=True):
-        lmList = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
 
@@ -40,11 +42,28 @@ class handDetector():
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 #print(id, cx, cy)
-                lmList.append([id, cx, cy])
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-        return lmList
+        return self.lmList
+
+    def fingersUp(self):
+        fingers = []
+
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 Fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
 
 def main():
     pTime = 0
