a3321f89621f205e666f4183fd5003d9809b0c38
Pavani Edirisinghe <eg245148@engug.ruh.ac.lk>
2026-06-11 08:34:12 +0530

    feat: Implement bicep curl state machine and integrate dynamic video scaling

--- PoseEstimationProject/PoseModule.py
+++ PoseEstimationProject/PoseModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 class PoseDetector():
 
@@ -30,16 +31,47 @@ class PoseDetector():
         return img
 
     def findPosition(self, img, draw = True):
-        lmList = []
+        self.lmList = []
         if self.results.pose_landmarks:
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 h, w, c = img.shape
                 #print(id, lm)
                 cx, cy = int(lm.x*w), int(lm.y*h)
-                lmList.append([id, cx, cy])
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255,0,0), cv2.FILLED)
-        return lmList
+        return self.lmList
+
+    def findAngle(self, img, p1, p2, p3, draw = True):
+
+        # Get the landmarks
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+
+        # Calculate the Angle
+        angle = math.degrees(math.atan2(y3-y2, x3-x2)- math.atan2(y1-y2, x1-x2))
+
+        if angle < 0:
+            angle += 360
+
+        # print(angle)
+
+
+        # Draw
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (0, 255, 0), 3)
+            cv2.circle(img, (x1, y1), 10, (0,0,255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            # cv2.putText(img, str(int(angle)),(x2-50, y2+50),
+            #             cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 2)
+
+        return angle
 
 def main():
     cap = cv2.VideoCapture('PoseVideos/2.mp4')
